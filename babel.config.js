74b3c0a98e18d4a7aa946f3f749ec1630e97ae26
ryutaro maeda <r.maeda@asaburo.com>
2024-06-26 12:53:48 +0900

    fix babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -4,17 +4,15 @@ module.exports = function(api) {
   var isDevelopmentEnv = api.env('development')
   var isProductionEnv = api.env('production')
   var isTestEnv = api.env('test')
-
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
-      'Please specify a valid `NODE_ENV` or ' +
+        'Please specify a valid `NODE_ENV` or ' +
         '`BABEL_ENV` environment variables. Valid values are "development", ' +
         '"test", and "production". Instead, received: ' +
         JSON.stringify(currentEnv) +
         '.'
     )
   }
-
   return {
     presets: [
       isTestEnv && [
@@ -54,13 +52,13 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-proposal-private-methods',
+        '@babel/plugin-transform-private-methods',
         {
           loose: true
         }
       ],
       [
-        '@babel/plugin-proposal-private-property-in-object',
+        '@babel/plugin-transform-private-property-in-object',
         {
           loose: true
         }
